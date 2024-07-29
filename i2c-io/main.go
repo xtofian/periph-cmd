e2674b5ebb848a687beaa1bfdfc6b92c696110ff
Christoph Kern <xtof@xtof.org>
2024-07-29 08:27:44 -0700

    i2c-io: Optionally record i2c transactions. Support non-register-based i2c devices.
    In verbose mode, i2c-io displays contents of
    i2c transactions in an xxd(1)-like format, as well
    as i2ctest recorded operations. The latter is
    useful for creating unit test using
    i2ctest.Playback.
    
    Specifying a register to address via -r is now
    optional.  This allows i2c-io to be used with
    non-register-based devics like Atlas Scientific
    EZO pH/ORP sensors.

--- i2c-io/main.go
+++ i2c-io/main.go
@@ -16,9 +16,41 @@ import (
 
 	"periph.io/x/conn/v3/i2c"
 	"periph.io/x/conn/v3/i2c/i2creg"
+	"periph.io/x/conn/v3/i2c/i2ctest"
 	"periph.io/x/conn/v3/physic"
 	"periph.io/x/host/v3"
 )
+
+// adapted from https://gist.github.com/chmike/05da938833328a9a94e02506922f2e7b
+func xxd(b []byte) string {
+	var a [16]byte
+	s := ""
+	n := (len(b) + 15) &^ 15
+	for i := 0; i < n; i++ {
+		if i%16 == 0 {
+			s += fmt.Sprintf("%4d", i)
+		}
+		if i%8 == 0 {
+			s += " "
+		}
+		if i < len(b) {
+			s += fmt.Sprintf(" %02X", b[i])
+		} else {
+			s += "   "
+		}
+		if i >= len(b) {
+			a[i%16] = ' '
+		} else if b[i] < 32 || b[i] > 126 {
+			a[i%16] = '.'
+		} else {
+			a[i%16] = b[i]
+		}
+		if i%16 == 15 {
+			s += fmt.Sprintf("  %s\n", string(a[:]))
+		}
+	}
+	return s
+}
 
 func mainImpl() error {
 	addr := flag.Int("a", -1, "I²C device address to query")
@@ -42,8 +74,8 @@ func mainImpl() error {
 	if *addr < 0 || *addr >= 1<<9 {
 		return fmt.Errorf("-a is required and must be between 0 and %d", 1<<9-1)
 	}
-	if *reg < 0 || *reg > 255 {
-		return errors.New("-r must be between 0 and 255")
+	if *reg < -1 || *reg > 255 {
+		return errors.New("-r must be between -1 and 255")
 	}
 	if *l <= 0 || *l > 255 {
 		return errors.New("-l must be between 1 and 255")
@@ -58,10 +90,14 @@ func mainImpl() error {
 		if flag.NArg() == 0 {
 			return errors.New("specify data to write as a list of hex encoded bytes")
 		}
-		buf = make([]byte, 1, flag.NArg()+1)
-		buf[0] = byte(*reg)
+		if *reg >= 0 {
+			buf = make([]byte, 1, flag.NArg()+1)
+			buf[0] = byte(*reg)
+		} else {
+			buf = make([]byte, 0, flag.NArg())
+		}
 		for _, a := range flag.Args() {
-			b, err := strconv.ParseUint(a, 0, 8)
+			b, err := strconv.ParseUint(a, 16, 8)
 			if err != nil {
 				return err
 			}
@@ -90,11 +126,20 @@ func mainImpl() error {
 			log.Printf("Using pins SCL: %s  SDA: %s", p.SCL(), p.SDA())
 		}
 	}
-	d := i2c.Dev{Bus: bus, Addr: uint16(*addr)}
+	i2cRecorder := i2ctest.Record{Bus: bus}
+
+	d := i2c.Dev{Bus: &i2cRecorder, Addr: uint16(*addr)}
 	if *write {
+		if *verbose {
+			log.Printf("Writing: %#v", buf)
+		}
 		_, err = d.Write(buf)
 	} else {
-		if err = d.Tx([]byte{byte(*reg)}, buf); err != nil {
+		req := []byte{}
+		if *reg >= 0 {
+			req = []byte{byte(*reg)}
+		}
+		if err = d.Tx(req, buf); err != nil {
 			return err
 		}
 		for i, b := range buf {
@@ -108,6 +153,15 @@ func mainImpl() error {
 			}
 		}
 		_, err = fmt.Print("\n")
+	}
+
+	if *verbose {
+		log.Printf("recorded ops:\n%#v", i2cRecorder.Ops)
+
+		for _, op := range i2cRecorder.Ops {
+			log.Printf("  W:\n%s", xxd(op.W))
+			log.Printf("  R:\n%s", xxd(op.R))
+		}
 	}
 	return err
 }
